0cc608b7d388a3f3903a416940ec2c1a5cf6139b
lain <erqleyz@gmail.com>
2025-03-01 12:45:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', (event) => {
+    const clickId = event.target.id;
+    switch (clickId) {
+      case 'yes':
+        
+    }
+    }
+  });
+});
