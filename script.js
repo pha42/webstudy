9283599c416408e6693058b918c413ae4e6ade8b
lain <erqleyz@gmail.com>
2025-03-01 12:56:33 +0300

    button

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.addEventListener('click', (event) => {
+document.addEventListener("DOMContentLoaded", () => {
+  const yesSound = new Audio("media/yes.mp3");
+  const noSound = new Audio("media/no.mp3");
+  yesSound.volume = 0.2;
+  noSound.volume = 0.2;
+
+  document.body.addEventListener("click", (event) => {
     const clickId = event.target.id;
     switch (clickId) {
-      case 'yes':
-        
-    }
+      case "yes":
+        yesSound.play();
+        console.log("yes");
+        break;
+      case "no":
+        noSound.play();
+        console.log("no");
+        break;
     }
   });
 });
